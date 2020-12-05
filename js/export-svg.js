78985246a3e716b08b53be93e56c85b134de9695
Eyal Perry <eyal.perry88@gmail.com>
2020-12-05 17:44:52 -0500

    drawing edges to svg

--- js/export-svg.js
+++ js/export-svg.js
@@ -23,6 +23,23 @@ function exportToSvg(globals) {
     }
   }
 
+  for (var e in traceHistoryEdges) {
+    for (var i = 0; i < traceHistoryEdges[e].length; i++) {
+      for (var j = 0; j < 4; j += 2) {
+        var x = sx * traceHistoryEdges[e][i][j];
+        var y = sy * traceHistoryEdges[e][i][j + 1];
+        if (x < minX)
+          minX = x;
+        if (x > maxX)
+          maxX = x;
+        if (y < minY)
+          minY = y;
+        if (y > maxY)
+          maxY = y;
+      }
+    }
+  }
+
   var width = maxX - minX;
   var height = maxY - minY;
 
@@ -32,9 +49,28 @@ function exportToSvg(globals) {
   }
 
   var ctx2Svg = new C2S(width, height);
+  ctx2Svg.translate(-minX, -minY);
 
   ctx2Svg.fillStyle = 'white'
   // ctx2Svg.fillRect(0, 0, width, height);
+
+  var e_idx = 0;
+  for (var e in traceHistoryEdges) {
+    ctx2Svg.fillStyle = traceColors[e_idx % traceColors.length];
+    ctx2Svg.beginPath();
+    // relative positions because trace should move when we move the linkage
+    ctx2Svg.moveTo(sx*traceHistoryEdges[e][0][0], sy*traceHistoryEdges[e][0][1]);
+    for (var i = 1; i < traceHistoryEdges[e].length; i++) {
+      ctx2Svg.lineTo(sx*traceHistoryEdges[e][i][0], sy*traceHistoryEdges[e][i][1]);
+    }
+    ctx2Svg.lineTo(sx*traceHistoryEdges[e][traceHistoryEdges[e].length-1][2], sy*traceHistoryEdges[e][traceHistoryEdges[e].length-1][3]);
+    for (var i = traceHistoryEdges[e].length - 2; i >= 0; i--) {
+      ctx2Svg.lineTo(sx*traceHistoryEdges[e][i][2], sy*traceHistoryEdges[e][i][3]);
+    }
+    ctx2Svg.closePath();
+    ctx2Svg.fill();
+    e_idx += 1;
+  }
 
   var p_idx = 0;
   for (var p in traceHistory) {
@@ -42,9 +78,9 @@ function exportToSvg(globals) {
     ctx2Svg.strokeStyle = traceColors[p_idx % traceColors.length];
     ctx2Svg.beginPath();
     // relative positions because trace should move when we move the linkage
-    ctx2Svg.moveTo(sx*traceHistory[p][0][0] - minX, sy*traceHistory[p][0][1] - minY);
+    ctx2Svg.moveTo(sx*traceHistory[p][0][0], sy*traceHistory[p][0][1]);
     for (var i = 1; i < traceHistory[p].length; i++) {
-      ctx2Svg.lineTo(sx*traceHistory[p][i][0] - minX, sy*traceHistory[p][i][1] - minY);
+      ctx2Svg.lineTo(sx*traceHistory[p][i][0], sy*traceHistory[p][i][1]);
     }
     ctx2Svg.stroke();
     p_idx += 1;
